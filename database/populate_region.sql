79b8782a8059b32b39b558177232d1bbbf226f75
Augusto Aguayo <hostile.command@gmail.com>
2025-09-29 18:13:46 -0300

    sql fixes + updated gitignore

--- database/populate_region.sql
+++ database/populate_region.sql
@@ -1,3 +1,4 @@
+USE tarea2;
 INSERT INTO region (id, nombre) VALUES (1,'Región de Tarapacá');
 INSERT INTO region (id, nombre) VALUES (2,'Región de Antofagasta');
 INSERT INTO region (id, nombre) VALUES (3,'Región de Atacama');
